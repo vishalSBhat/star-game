6e98dc5aa343d2ad1d5891c32432cad41c99f105
vishalSBhat <vishalbhat97@gmail.com>
2020-04-27 21:31:14 +0530

    Updates

---
+++ precache-manifest.f350b77d6b2eb6a7593a21dbbeec9cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dad461cc385f4254fd5fea857a2026",
+    "url": "/star-game/index.html"
+  },
+  {
+    "revision": "9cc006eab3771139ec3b",
+    "url": "/star-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bb4aa58dbe9d18f7004d",
+    "url": "/star-game/static/js/2.4eac8ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-game/static/js/2.4eac8ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc006eab3771139ec3b",
+    "url": "/star-game/static/js/main.1b56d0b2.chunk.js"
+  },
+  {
+    "revision": "e9d73905950a3c432037",
+    "url": "/star-game/static/js/runtime-main.64a11a88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
